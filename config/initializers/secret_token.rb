2fab4ccab9522aa3834855a401aec2149174fc48
Markus Grobelin <markus@fear-group.com>
2014-03-24 15:37:07 +0100

    initial rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Openmetrics::Application.config.secret_key_base = 'eab82a6e358e62d48663f574f1e2bbdb24f986f5eeed101d97c7eb4bb384f841ebaf02b1fd769543206c615385d4300b71b36f2ff87ae630492d5d33fc27433f'
