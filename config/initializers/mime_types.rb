f732b6e1e197aa62e1320a1aff84b3bc17027ba8
Markus Grobelin <grobi@koppzu.de>
2014-04-06 11:21:09 +0200

    added proper mimetypes for (bootstrap) web fonts

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,3 +3,9 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
+
+# tell Rack (and Sprockets) about modern font MIME types:
+Mime::Type.register "application/x-font-woff", :woff
+Mime::Type.register "application/x-font-ttf", :ttf
+Mime::Type.register "application/vnd.ms-fontobject", :eot
+Mime::Type.register "image/svg+xml", :svg
