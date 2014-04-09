a742d7de42d4ecaec63b32dcaf67f38d289ba760
Markus Grobelin <grobi@koppzu.de>
2014-04-09 02:53:23 +0200

    added services added system running_services and active services

--- app/models/system.rb
+++ app/models/system.rb
@@ -1,9 +1,10 @@
 class System < ActiveRecord::Base
   has_many :running_services
   has_many :services, :through => :running_services
-  has_many :active_services, :through => :running_services,
-            :class_name => 'Service',
-            :source => :system
+  # has_many :active_services, ->{ where(active: true).order(:name) },
+  #           through: :running_services,
+  #           class_name: 'Service',
+  #           source: :system
 
   validates :name, :presence => true, length: {minimum: 3},
                    :uniqueness => true, :format => { :with => /[A-Za-z0-9::space::]+/ }
