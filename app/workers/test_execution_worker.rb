7b244bef88136a515cef484c48e7cbdecc3f9a28
Markus Grobelin <grobi@koppzu.de>
2014-05-04 00:51:28 +0200

    execute test plans

--- app/workers/test_execution_worker.rb
+++ app/workers/test_execution_worker.rb
@@ -1,5 +1,6 @@
 require 'selenium-webdriver'
 require 'webtest_automagick'
+require 'systemu'
 
 class TestExecutionWorker
   include Sidekiq::Worker
@@ -10,41 +11,48 @@ class TestExecutionWorker
 
   def perform(testplan_id)
     tp = TestPlan.find(testplan_id)
-    #run_test_plan1
     Dir.exist?(TMPDIR) || system('mkdir', '-p', "#{TMPDIR}")
-    test_exectution_items = prepare(tp)
-    #scan_services!(target)
+    test_execution_items = prepare(tp)
+    test_execution_items.each { |tei| execute(tei) }
   end
 
   # reads test plans test_items and generate executables depending on item format/markup
   def prepare(tp)
+    executable_files = []
     items = tp.test_items
     items.each_with_index { |item, n|
-      tmpfile = ` mktemp -p #{TMPDIR} #{tp.id}_#{n}_#{item.id}.XXXXXX`
+      tmpfile = `mktemp -p #{TMPDIR} #{tp.id}_#{n}_#{item.id}.XXXXXX`
       executable_markup = if item.type == 'TestCase' && item.format == 'selenese'
                             selenese_to_webdriver(item.markup)
                           else
                             item.markup # default item.markup
                           end
 
-      File.open(tmpfile, 'w') { |f| f.write(executable_markup) }
+      # add executable header (shebang) and write to file
+      header = ''
+      case item.markup
+        when 'selense'
+          header= %q(#!/usr/bin/env ruby)
+        when 'bash'
+          header = %q(#!/bin/bash)
+      end
+
+      begin
+        File.open(tmpfile, 'w') { |f| f.write(header+executable_markup) }
+        executable_files.push(tmpfile)
+      rescue
+        logger.error("Couldn't write executable file")
+      end
     }
+    return executable_files
   end
 
-  def run_test_plan1
-
-    begin
-      driver = Selenium::WebDriver.for :firefox
-      driver.navigate.to "http://google.com"
-
-      element = driver.find_element(:name, 'q')
-      element.send_keys "Hello WebDriver!"
-      element.submit
-
-      logger.info('Title: ' + driver.title)
-    ensure
-      driver.quit
-    end
+  def execute(executable)
+    logger.info("executing #{executable.inspect}")
+    #cmd = "sh #{executable}"
+    #status, stdout, stderr = systemu cmd, 'env'=>{ 'OM_FOO' => "0b101010" }
+    status = system('sh', executable)
+    logger.info("status: #{status} (#{$?.exitstatus})")
   end
 
 end
--- lib/webtest_automagick.rb
+++ lib/webtest_automagick.rb
@@ -19,8 +19,49 @@ module WebtestAutomagick
       sel_commands.push([command, target, value])
     end
 
-    p sel_commands
-    sel_commands
-  end
+    wd='' #webdriver markup
 
+    # prepare head
+    wd << %Q[
+require "selenium-webdriver"
+driver = Selenium::WebDriver.for :firefox
+driver.navigate.to "http://google.com"
+]
+
+    sel_commands.each do |command, target, value|
+
+      # decide kind of element locator
+      # see http://selenium.googlecode.com/git/ide/main/src/content/selenium-core/reference.html#locators
+      # and http://rubydoc.info/gems/selenium-webdriver/0.0.28/Selenium/WebDriver/Find for
+      how=nil
+      what=nil
+      case target
+        when /^css=(.*)/
+          how = ':css'
+          what = $1
+        when /^\/{2}(.*)/ # xpath shortcut '//'
+          how = ':xpath'
+          what = $1
+        when /^xpath=(.*)/
+          how = ':xpath'
+          what = $1
+        when /^id(entifier)?=(.*)/
+          how = ':id'
+          what = $2
+      end
+
+      # see http://selenium.googlecode.com/git/docs/api/rb/Selenium/Client/GeneratedDriver.html
+      #wd << "#{command} ::: #{target} ::: #{value}"
+      case command
+        when /click|clickAndWait/
+          wd << "driver.find_element(#{how}, \"#{what}\").click\n"
+        when /open/
+          wd << "driver.open(\"#{target}\")\n"
+        when /type/
+          wd << "driver.find_element(#{how}, \"#{what}\").send_keys(\"#{value}\")\n"
+      end
+    end
+    wd << "driver.quit"
+    wd
+  end
 end
