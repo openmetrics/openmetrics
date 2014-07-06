ae9ea1ac828e07505db10551fbe9c32220a36745
Markus Grobelin <grobi@koppzu.de>
2014-07-06 18:44:26 +0200

    IpLookup and TestExecution make usage of Executable concern

--- app/models/concerns/executable.rb
+++ app/models/concerns/executable.rb
@@ -11,6 +11,35 @@ module Executable
 
   end
 
+
+  # create associated result for latter processing
+  def create_result()
+    if self.class.name == 'TestExecution'
+      TestExecutionResult.create!(test_execution: self)
+    elsif self.class.name == 'IpLookup'
+      IpLookupResult.create!(ip_lookup: self)
+    end
+  end
+
+
+  # try to perform async, otherwise fail with meaningful status
+  # use update_column here to bypass callbacks (were already within transaction here due to after_commit callback)
+  def schedule_execution()
+    begin
+      update_column :status, 5 #init with 'none' state, as scheduling may fail
+      if self.class.name == 'TestExecution'
+        update_column :job_id, TestExecutionWorker.perform_async(self.id, self.test_plan_id)
+      elsif self.class.name == 'IpLookup'
+        update_column :job_id, IpLookupWorker.perform_async(self.id)
+      end
+      update_column :status, EXECUTION_STATUS.key('scheduled')
+      logger.info "#{self.class.name} #{self.id} scheduled for execution (jid: #{self.job_id})."
+    rescue Exception => e
+      # job scheduling failed
+      logger.error "Failed to schedule #{self.class.name} #{self.id} due to exception #{e.message}"
+    end
+  end
+
   # virtual attribute execution time in millisecond precision, calculated from started_at and finished_at
   def duration
     return nil if self.started_at.nil? or self.finished_at.nil?
--- app/models/test_execution.rb
+++ app/models/test_execution.rb
@@ -1,28 +1,10 @@
 class TestExecution < ActiveRecord::Base
+  include Executable
+
   belongs_to :test_plan
   # TestExecutionItem belongs to TestExecution, but we currently use method test_execution_items
   # has_many :test_execution_items
   has_one :test_execution_result
-
-
-  # try to perform async, otherwise fail with meaningful status
-  # use update_column here to bypass callbacks (were already within transaction here due to after_commit callback)
-  include Executable
-  def schedule_execution()
-    begin
-      update_column :status, 5 #init with 'none' state, as scheduling may fail
-      update_column :job_id, TestExecutionWorker.perform_async(self.id, self.test_plan_id)
-      update_column :status, EXECUTION_STATUS.key('scheduled')
-      logger.info "TestPlan #{self.test_plan_id} scheduled for execution (jid: #{self.job_id})."
-    rescue Exception => e
-      # job scheduling failed
-      logger.error "Failed to schedule TestPlan #{self.test_plan_id} due to exception #{e.message}"
-    end
-  end
-  # create associated TestExecutionResult for latter processing
-  def create_result()
-    TestExecutionResult.create!(test_execution: self)
-  end
 
   # most recent test executions
   scope :recent, ->(num=5) { order('created_at DESC').limit(num) }
