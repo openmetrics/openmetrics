674eb16d2c3e5b0281f26aa9a426eedb905e6c10
Markus Grobelin <grobi@koppzu.de>
2014-11-14 14:37:19 +0100

    made projects searchable; dont underline links

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,10 +7,10 @@ class SearchesController < ApplicationController
     if @search_string != nil
       q = "%#{@search_string}%"
 
-      @test_plans = TestPlan.where("name ilike ? or description ilike ?", q, q)
+      @projects = Project.where("name ilike ? or description ilike ?", q, q)
       @systems = System.where("name ilike ? or fqdn ilike ?", q, q)
       @services = Service.where("name ilike ?", q)
-
+      @test_plans = TestPlan.where("name ilike ? or description ilike ?", q, q)
     end
 
     if request.xhr?
